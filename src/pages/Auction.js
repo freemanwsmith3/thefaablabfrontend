53d0c1e5ac050c4efec8cea23c67f8365abfe626
Freeman Smith <freemanwsmith3@gmail.com>
2024-08-29 11:34:57 -0500

    auction 200 fix

--- src/pages/Auction.js
+++ src/pages/Auction.js
@@ -7,7 +7,7 @@ function Auction({  }) {
     <div className='faab-container'>
     <div className='homeCard'>
         <h3 style={{ marginTop: '20px', fontSize: 45, lineHeight: '1.5' }}>2024 Auction Values</h3>
-        <h3 style={{ marginTop: '0px', fontSize: 25, lineHeight: '1' }}>Half PPR | 12 Team </h3>
+        <h3 style={{ marginTop: '0px', fontSize: 25, lineHeight: '1' }}>$200 Budget | Half PPR  </h3>
         <SingleCard week = {auction_week} />
         </div>
     </div>
