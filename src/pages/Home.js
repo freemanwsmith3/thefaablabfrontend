84879cfb3888f75b2b353ed18cf31226d785ab25
Freeman Smith <freemanwsmith3@gmail.com>
2024-12-03 09:43:40 -0600

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,7 +10,7 @@ function Home({ curWk }) {
   return (
     <div className='faab-container'>
       <div className='homeCard'>
-        <h3>Week {week - 27}</h3>
+        <h3>FAABLab is done for 2024, see you in 2025</h3>
         <SingleCard week={week} />
       </div>
     </div>
