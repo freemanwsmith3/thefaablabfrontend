660343ef0ebaa501b82912b73ed229feb08ba078
Freeman Smith <freemanwsmith3@gmail.com>
2025-12-09 16:34:45 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
     logPageView();
   }, []);
 
-  const [curWk, setCurWk] = useState(53);
+  const [curWk, setCurWk] = useState(54);
 
 const location = useLocation();
 const hideHeaderFooterRoutes = ['/toptargets', '/compact-dashboard'];
